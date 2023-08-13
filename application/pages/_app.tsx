0fb2ef676d6d96fefdceeeb82a91aefb1102e23d
BLK LUV [org] <info@blkluv.org>
2023-08-12 21:08:28 -0400

    Update _app.tsx

--- application/pages/_app.tsx
+++ application/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const activeChain = "mainnet";
+  const activeChain = "matic";
 
   return (
     <ThirdwebProvider activeChain={activeChain}>
